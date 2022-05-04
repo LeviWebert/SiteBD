0ad9c3bc21f3dd2b6f51fb2ecb61f7fd39578986
LeviWebert <levi.webert@ortmontreuil.fr>
2022-05-04 12:05:44 +0200

    inscription connexion

--- Modele/bd.utilisateur.inc.php
+++ Modele/bd.utilisateur.inc.php
@@ -10,11 +10,7 @@ function getUtilisateurs() {
         $req = $cnx->prepare("select * from utilisateur");
         $req->execute();
 
-        $ligne = $req->fetch(PDO::FETCH_ASSOC);
-        while ($ligne) {
-            $resultat[] = $ligne;
-            $ligne = $req->fetch(PDO::FETCH_ASSOC);
-        }
+        $resultat = $req->fetchAll(PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
         print "Erreur !: " . $e->getMessage();
         die();
@@ -39,6 +35,59 @@ function getUtilisateurByMailU($mailU) {
     return $resultat;
 }
 
+function addUtilisateur($mailU, $mdpU, $pseudoU) {
+    try {
+        $cnx = connexionPDO();
+
+        $mdpUCrypt = crypt($mdpU, "sel");
+        $req = $cnx->prepare("insert into utilisateur (mailU, mdpU, pseudoU) values(:mailU,:mdpU,:pseudoU)");
+        $req->bindValue(':mailU', $mailU, PDO::PARAM_STR);
+        $req->bindValue(':mdpU', $mdpUCrypt, PDO::PARAM_STR);
+        $req->bindValue(':pseudoU', $pseudoU, PDO::PARAM_STR);
+        
+        $resultat = $req->execute();
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function updtMdpUtilisateur($mailU, $mdpU) {
+    $resultat = -1;
+    try {
+        $cnx = connexionPDO();
+
+        $mdpUCrypt = crypt($mdpU, "sel");
+        $req = $cnx->prepare("update utilisateur set mdpU=:mdpU where mailU=:mailU");
+        $req->bindValue(':mailU', $mailU, PDO::PARAM_STR);
+        $req->bindValue(':mdpU', $mdpUCrypt, PDO::PARAM_STR);
+
+        $resultat = $req->execute();
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function updtPseudoUtilisateur($mailU, $pseudoU) {
+    $resultat = -1;
+    try {
+        $cnx = connexionPDO();
+
+        $req = $cnx->prepare("update utilisateur set pseudoU=:pseudoU where mailU=:mailU");
+        $req->bindValue(':mailU', $mailU, PDO::PARAM_STR);
+        $req->bindValue(':pseudoU', $pseudoU, PDO::PARAM_STR);
+
+        $resultat = $req->execute();
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
 if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
     // prog principal de test
     header('Content-Type:text/plain');
@@ -46,9 +95,10 @@ if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
     echo "getUtilisateurs() : \n";
     print_r(getUtilisateurs());
 
-    echo "getUtilisateurByMailU('mathieu.capliez@gmail.com') : \n";
+    echo "getUtilisateurByMailU(\"mathieu.capliez@gmail.com\") : \n";
     print_r(getUtilisateurByMailU("mathieu.capliez@gmail.com"));
 
-
+    echo "addUtilisateur(\"mathieu.capliez3@gmail.com\") : \n";
+    addUtilisateur("mathieu.capliez3@gmail.com", "Passe1?", "mat");
 }
 ?>
